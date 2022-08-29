eba52f6ff25cd319696a6394ff4f042ec09ca1ff
Ricardo Nunes <ricardodnn1@gmail.com>
2022-08-29 10:57:11 -0300

    Form Reactive

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
-﻿//Install express server
-const express = require('express');
+﻿const express = require('express');
 const path = require('path');
-
+const nomeApp = process.env.npm_package_name;
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname+'/dist/teste-mkdata-frontend'));
+app.use(express.static(`${__dirname}/dist/${nomeApp}`));
 
-app.get('/*', (req, res) =>
-    res.sendFile(__dirname+'/dist/teste-mkdata-frontend/index.html'),
-);
+app.get('/*', (req, res) => {
+res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
+});
 
-// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
