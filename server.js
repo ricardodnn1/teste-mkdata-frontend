b83c12bb5171f12a43d438827da6c4eeac4c0901
Ricardo Nunes <ricardodnn1@gmail.com>
2022-08-29 11:41:23 -0300

    Heroku

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
-﻿const express = require('express');
+﻿//Install express server
+const express = require('express');
 const path = require('path');
+
 const app = express();
 
-app.use(express.static(__dirname + '/dist/teste-mkdata-frontend'));
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/teste-mkdata-frontend'));
 
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname + '/dist/teste-mkdata-frontend/index.html'));
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/teste-mkdata-frontend/'}),
+);
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080, () => {
+    console.log("Running!")
 });
-
-app.listen(process.env.PORT || 8080);
